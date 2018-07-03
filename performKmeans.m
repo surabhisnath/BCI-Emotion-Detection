786fc7908bf67006cd9b3e839497b695f39a929e
Surabhi S Nath <surabhi16271@iiitd.ac.in>
2018-07-03 16:38:07 +0530

    Removed bug

--- performKmeans.m
+++ performKmeans.m
@@ -1,18 +1,37 @@
-function answerclass = performKmeans(resized_alpha, answerclass)
+function [answerclass,sizes] = performKmeans(resized_alpha, answerclass, sizes)
     if size(resized_alpha,1) <= 20
-        disp('hi')
-        size(resized_alpha,1)
-        t = getGlobalx()
+        t = getGlobalx();    % get counter
+        sizes(1,t) = size(resized_alpha,1);
         answerclass(resized_alpha(:,1)) = t;
         setGlobalx(t+1);
         return;
     else
-        disp('hello')
-        alpha_idx1 = kmedoids(resized_alpha,2);
+        alpha_idx1 = kmedoids(resized_alpha(:,2:129),2);
         resized_alpha1 = resized_alpha(find(alpha_idx1==1),:);
         resized_alpha2 = resized_alpha(find(alpha_idx1==2),:);
     
-        answerclass = performKmeans(resized_alpha1, answerclass);
-        answerclass = performKmeans(resized_alpha2, answerclass);
+        [answerclass,sizes] = performKmeans(resized_alpha1, answerclass, sizes);
+        [answerclass,sizes] = performKmeans(resized_alpha2, answerclass, sizes);
     end
 end
+
+% while(size(find(alpha_idx1==1),1)>20 | size(find(alpha_idx1==2),1)>20)
+%     
+%     if size(find(alpha_idx1==1),1)>20 & size(find(alpha_idx1==2),1)>20
+%         resized_alpha1 = resized_alpha(find(alpha_idx1==1),1);
+%         resized_alpha2 = resized_alpha(find(alpha_idx1==2),1);
+%         resized_alpha
+%         alpha_idx1 = kmedoids(resized_alpha,2,'Distance','spearman');
+%     elseif size(find(alpha_idx1==1),1)>20
+%         find(alpha_idx1==2)
+%         resized_alpha = resized_alpha(find(alpha_idx1==1),1);
+%         alpha_idx1 = kmedoids(resized_alpha,2,'Distance','spearman');
+%         
+%     elseif size(find(alpha_idx1==2),1)>20
+%         store alpha_idx1==1
+%         resized_alpha = resized_alpha(surafind(alpha_idx1==2),1);
+%         alpha_idx1 = kmedoids(resized_alpha,2,'Distance','spearman');
+%     else
+%         store both
+%     end
+% end
